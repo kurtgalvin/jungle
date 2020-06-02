af65cea795ce551769a47c125ba09f7148af78cd
Kurt Galvin <kurtgalvin@gmail.com>
2020-06-02 23:38:36 +0000

    get to product page spec

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-
+RSpec.feature "ProductDetails", type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -21,7 +20,9 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     # ACT
     visit root_path
 
-    # VERIFY
-    expect(page).to have_css 'article.product', count: 10
+    product = page.first("article.product")
+    product.click_link "Details"
+
+    expect(page).to have_css 'article.product-detail'
   end
 end
